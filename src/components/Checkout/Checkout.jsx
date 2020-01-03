4281df94087e04fb0cb6cfcec7e34387644d8495
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-01-03 13:42:06 +0200

    adding local state for success message handling to checkout component. Redirecting to home page after successful payment

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,51 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCartItems, selectCartTotal } from "../../redux/CartSelectors";
 import { clearCart } from "../../redux/actions/CartActions";
 import CheckoutItem from "../CheckoutItem/CheckoutItem";
+import { Redirect } from "react-router-dom";
 import StripeButton from "../UI/StripeButton/StripeButton";
 import PropTypes from "prop-types";
 import "./Checkout.css";
 
-const Checkout = ({ cartItems, total, clearCart }) => {
-  const onPaymentCompleted = () => {
-    clearCart();
+class Checkout extends Component {
+  state = {
+    showSuccessPaymentMessage: false,
+    redirect: false
   };
-  return (
-    <div className="checkout-page">
-      <div className="checkout-header">
-        <div className="header-block">
-          <span>Product</span>
-        </div>
-        <div className="header-block">
-          <span>Description</span>
-        </div>
-        <div className="header-block">
-          <span>Quantity</span>
-        </div>
-        <div className="header-block">
-          <span>Single Price</span>
-        </div>
-        <div className="header-block">
-          <span>Remove</span>
-        </div>
+  onPaymentCompleted = () => {
+    this.props.clearCart();
+    this.setState({
+      showSuccessPaymentMessage: true
+    });
+    setTimeout(() => {
+      this.setState({ redirect: true });
+    }, 4000);
+  };
+  render() {
+    return this.state.showSuccessPaymentMessage ? (
+      <div className="container text-center">
+        <h2>
+          Your payment was successful. Expect your order withing the next 45
+          minutes.
+        </h2>
+        <p className="redirect-message">Redirecting to home page...</p>
+
+        {this.state.redirect && <Redirect to="/" />}
       </div>
-      {cartItems.map(item => {
-        return <CheckoutItem key={item.id} cartItem={item} />;
-      })}
-      <div className="total">
-        <span>${total}</span>
+    ) : (
+      <div className="checkout-page">
+        <div className="checkout-header">
+          <div className="header-block">
+            <span>Product</span>
+          </div>
+          <div className="header-block">
+            <span>Description</span>
+          </div>
+          <div className="header-block">
+            <span>Quantity</span>
+          </div>
+          <div className="header-block">
+            <span>Single Price</span>
+          </div>
+          <div className="header-block">
+            <span>Remove</span>
+          </div>
+        </div>
+        {this.props.cartItems.map(item => {
+          return <CheckoutItem key={item.id} cartItem={item} />;
+        })}
+        <div className="total">
+          <span>${this.props.total}</span>
+        </div>
+        <div className="card-info">
+          * Use the following TEST credit card for payments *
+          <br />
+          4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        </div>
+        <StripeButton
+          price={this.props.total}
+          onPaymentCompleted={this.onPaymentCompleted}
+        />
       </div>
-      <div className="card-info">
-        * Use the following TEST credit card for payments *
-        <br />
-        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-      </div>
-      <StripeButton price={total} onPaymentCompleted={onPaymentCompleted} />
-    </div>
-  );
-};
+    );
+  }
+}
 
 Checkout.propTypes = {
   cartItems: PropTypes.arrayOf(PropTypes.object).isRequired,
